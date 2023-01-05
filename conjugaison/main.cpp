c224f98e58ad0a5aece4015411304473ee70d22c
Camel Djoulako <cameldjoulako17@gmail.com>
2023-01-05 09:42:13 +0100

    exo conjugaison

---
+++ conjugaison/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char verbe[20];
+    int tailleVerbe;
+
+    cout << "entrer votre verbe" << endl;
+
+    cin >> verbe;
+
+    tailleVerbe = strlen(verbe);
+
+    if(tailleVerbe <= 2 || verbe[i-1] != 'r' || verbe[i-2] != 'e') {
+        cout << "Le verbe n'est pas du premier groupe" << endl;
+    } else {
+
+    }
+
+
+    return 0;
+}
