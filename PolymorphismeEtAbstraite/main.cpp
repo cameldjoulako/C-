d6f0c189ae9ca78844ec8759ab9a81335700b9cd
Camel Djoulako <cameldjoulako17@gmail.com>
2023-01-24 13:17:22 +0100

    classe abstraite

--- PolymorphismeEtAbstraite/main.cpp
+++ PolymorphismeEtAbstraite/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include <math.h>
+
 using namespace std;
 
-    class Rectangle {
+//Methode virtuel
+    /*class Rectangle {
         protected:
             double largeur, longueur;
         public:
@@ -37,14 +40,83 @@ using namespace std;
                 return surface;
             }
 
+    };*/
+
+
+    //LES CLASSES ABSTRAITES EN C++
+
+    /*syntaxe methode abstraite
+
+    virtual typeDeRetour nomDeLaMethode(parametres) = 0;
+
+    */
+
+    class Figure {
+        public:
+            virtual double surface() = 0;
+            virtual double perimetre() = 0;
     };
+
+
+
+class Rectangle : public Figure {
+    protected:
+        double largeur, longueur;
+    public:
+        Rectangle(double x=0, double y=0) {
+            largeur = x;
+            longueur = y;
+        }
+
+        double surface() {
+            double calSurface = longueur * largeur;
+
+            return calSurface;
+        }
+        double perimetre() {
+            double calPerimetre = 2*longueur*largeur;
+            return calPerimetre;
+        }
+
+};
+
+
+class Cercle : public Figure {
+    protected:
+        double rayon;
+    public:
+        Cercle(double r=0):rayon(r){}
+
+        double surface() {
+            double calSurface = rayon * rayon *  M_PI;
+
+            return calSurface;
+        }
+        double perimetre() {
+            double calPerimetre = 2*rayon*M_PI;
+            return calPerimetre;
+        }
+
+};
+
+class Losange : public Figure {
+    protected:
+        double cote, diagonale;
+    public:
+        Losange(double x=0, double y=0) : cote(x), diagonale(y) {}
+
+        double perimetre() {
+            return (4*cote);
+        }
+
+};
 
 
 
 
 int main()
 {
-    cout << "POLYMORPHISME" << endl;
+    /*cout << "POLYMORPHISME" << endl;*/
 
     /*Rectangle rec(2, 3);
 
@@ -61,11 +133,28 @@ int main()
     cout << "Surface de la boite: " << rec->surface() << endl;
     */
 
-    Rectangle *rec
+    /*Rectangle *rec
     rec = new Rectangle3D(2, 3, 4);
-    delete rec;
+    delete rec;*/
 
 
+    cout << "CLASSE ABSTRAITE" << endl;
+
+    Figure *f;
+
+    f = new Cercle(1);
+
+    cout << "Perimetre du cercle : " << f->perimetre() << endl;
+    cout << "surface du cercle : " << f->surface() << endl;
+
+    delete f;
+
+    f = new Rectangle(2, 3);
+    cout << "surface du rectangle : " << f->surface() << endl;
+    delete f;
+
+
+    Figure * dessin[4]
 
 
 
